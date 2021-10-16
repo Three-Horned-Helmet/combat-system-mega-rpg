5f896bb18041e3e4cca5210a2ae05520f98923c3
Markus Heldrup <mah@teleplan.no>
2021-10-16 18:37:03 +0200

    restructuring of folders

--- dev/dev.js
+++ dev/dev.js
@@ -1,6 +1,6 @@
 const { askQuestion } = require("./helper")
-const { GameEngine } = require("./combat-system/GameEngine")
-const { MessageEngine } = require("./combat-system/MessageEngine")
+const { GameEngine } = require("../combat-system/GameEngine")
+const { MessageEngine } = require("./MessageEngine")
 const {playerOne, playerTwo, playerTree, playerFour} = require("./exampleUsers")
 
 const startGame = async () => {
