5c92cc87a391f5edfc3f1f2051f36bfb46a5a6a1
Markus Heldrup <mah@teleplan.no>
2021-11-20 11:51:38 +0100

    added allClasses exported object

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { GameEngine } = require("./combat-system/GameEngine")
 const { AbilityEngine } = require("./combat-system/AbilityEngine")
+
 const { DefaultClass } = require("./combat-system/classes/DefaultClass")
 const { UserArmy } = require("./combat-system/classes/NPC/UserArmy")
 const { Mage } = require("./combat-system/classes/Mage")
@@ -7,6 +8,31 @@ const { Ranger } = require("./combat-system/classes/Ranger")
 const { Warrior } = require("./combat-system/classes/Warrior")
 const { Shaman } = require("./combat-system/classes/Shaman")
 
-const allClasses = [DefaultClass, UserArmy, Mage, Ranger, Warrior, Shaman]
+const allClasses = { 
+    DefaultClass: {
+        class: DefaultClass,
+        constants: require("./combat-system/config/default-class-constants.json")
+    }, 
+    UserArmy: {
+        class: UserArmy,
+        constants: require("./combat-system/config/NPC/user-army-constants.json")
+    }, 
+    Mage: {
+        class: Mage,
+        constants: require("./combat-system/config/mage-constants.json")
+    }, 
+    Ranger: {
+        class: Ranger,
+        constants: require("./combat-system/config/ranger-constants.json")
+    }, 
+    Warrior: {
+        class: Warrior,
+        constants: require("./combat-system/config/warrior-constants.json")
+    }, 
+    Shaman: {
+        class: Shaman,
+        constants: require("./combat-system/config/shaman-constants.json")
+    }
+}
 
 module.exports = { GameEngine, AbilityEngine, allClasses }
