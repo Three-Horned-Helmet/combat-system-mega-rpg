3c4c35fb5e5633bd380dd70a84fad47620c8a85f
Markus Heldrup <mah@teleplan.no>
2021-11-20 12:00:20 +0100

    fixed all classes info

--- index.js
+++ index.js
@@ -11,28 +11,29 @@ const { Shaman } = require("./combat-system/classes/Shaman")
 const allClasses = { 
     DefaultClass: {
         class: DefaultClass,
-        constants: require("./combat-system/config/default-class-constants.json")
+        ...(require("./combat-system/config/default-class-constants.json"))
     }, 
     UserArmy: {
         class: UserArmy,
-        constants: require("./combat-system/config/NPC/user-army-constants.json")
+        ...(require("./combat-system/config/NPC/user-army-constants.json"))
     }, 
     Mage: {
         class: Mage,
-        constants: require("./combat-system/config/mage-constants.json")
+        ...(require("./combat-system/config/mage-constants.json"))
     }, 
     Ranger: {
         class: Ranger,
-        constants: require("./combat-system/config/ranger-constants.json")
+        ...(require("./combat-system/config/ranger-constants.json"))
     }, 
     Warrior: {
         class: Warrior,
-        constants: require("./combat-system/config/warrior-constants.json")
+        ...(require("./combat-system/config/warrior-constants.json"))
     }, 
     Shaman: {
         class: Shaman,
-        constants: require("./combat-system/config/shaman-constants.json")
+        ...(require("./combat-system/config/shaman-constants.json"))
     }
 }
+console.log("classes", allClasses)
 
 module.exports = { GameEngine, AbilityEngine, allClasses }
